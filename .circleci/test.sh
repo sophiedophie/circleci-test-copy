fa03187c57d789ddbc0d575cb13e534a4bea0ecb
sophiedophie <seohee.sophie.kwon@gmail.com>
2019-11-06 15:35:48 +0900

    sigh

---
+++ .circleci/test.sh
@@ -0,0 +1,10 @@
+Add-Type -AssemblyName System.web
+$raw_password = [System.Web.Security.Membership]::GeneratePassword(42, 10)
+$password = ConvertTo-SecureString $raw_password -AsPlainText -Force
+Set-LocalUser -Name "circleci" -Password $password
+$username = "circleci"
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
+$job = Start-Job -Credential $cred -ScriptBlock {
+  Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3" -All -NoRestart
+}
+$job | Receive-Job -Wait
